bc5dcf06be758ab2392238438d54a56ef14cd10d
doghouch <aaronryan2001@gmail.com>
2013-12-23 23:30:52 -0500

    Create final.php

---
+++ final.php
@@ -0,0 +1,25 @@
+<head>
+<title>SkinPerish</title>
+<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="css/bootstrap-responsive.css">
+<link rel="stylesheet" type="text/css" href="css/bootstrap-responsive.min.css">
+<center>
+    <form action="final.php">
+        <fieldset>
+            <legend>SkinPerish</legend>
+            <label></label>
+            </div>
+<center>
+   
+		
+			<b><img src="https://minotar.net/helm/<?php echo $_POST["username"]; ?>/100.png"></b>
+<br><br>
+ <code>Player "<?php echo $_POST["username"]; ?>"</code>
+		</div>
+       
+            
+<br>
+
+        </fieldset>
+    </form>
+<a href="/" class="btn btn-success">Go back</a>
